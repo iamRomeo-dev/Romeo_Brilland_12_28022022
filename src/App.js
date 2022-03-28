490b5605a560f0efabb73519d4ca25853830428f
iamRomeo-dev <romeo.brilland@live.fr>
2022-03-28 15:43:51 +0200

    feat: :sparkles: Display the layout

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@
 import "twin.macro";
 
 import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
-import { Home, Home1 } from "./Home";
+import { Layout } from "./Layout";
+import { Home } from "./Home";
 
 export const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/a" element={<Home1 />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <Layout />
       </BrowserRouter>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,5 @@
 import "twin.macro";
 
 export const Home = () => {
-  return (
-    <>
-      <h2>lferpgergzùgz;tk</h2>
-      <h1>totototototototo</h1>
-    </>
-  );
-};
-
-export const Home1 = () => {
-  return <h1>df g fge get g</h1>;
+  return <h1>eeee</h1>;
 };
